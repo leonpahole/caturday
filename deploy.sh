8036625b829c6a55ee8ebad14c4d41be14f4167f
Leon Pahole <leonnieen@gmail.com>
2020-04-05 16:35:35 +0200

    Add production deployment files

---
+++ deploy.sh
@@ -0,0 +1,53 @@
+set -u
+
+CONFIG_FILE=deploy.env
+
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Configuration file $CONFIG_FILE does not exist. Please create it."
+    exit 1
+fi
+
+# read configuration from configuration file
+source $CONFIG_FILE
+
+echo 
+echo "### CREATING DIRECTORY ####"
+
+
+ssh $SERVER_USERNAME@$SERVER_IP "mkdir -p $PROJECT_DIRECTORY"
+
+
+echo 
+echo "### COPY COMPOSE FILE ####"
+
+read -p 'Copy docker-compose.prod.yml? (y/n) [n]:' COPY_COMPOSE
+
+if [[ $COPY_COMPOSE == "y" || $COPY_COMPOSE == "Y" ]]; then
+    scp docker-compose.prod.yml $SERVER_USERNAME@$SERVER_IP:$PROJECT_DIRECTORY
+fi
+
+echo 
+echo "### COPY ENV FILE ####"
+
+read -p 'Copy docker .env? (y/n) [n]:' COPY_ENV
+
+if [[ $COPY_ENV == "y" || $COPY_ENV == "Y" ]]; then
+    scp .env $SERVER_USERNAME@$SERVER_IP:$PROJECT_DIRECTORY
+fi
+
+echo 
+echo "### COPY PERMISSION SCRIPT ####"
+
+scp fix_permissions_for_production.sh $SERVER_USERNAME@$SERVER_IP:$PROJECT_DIRECTORY
+
+echo
+echo "### RUNNING DEPLOYMENT ###"
+
+ssh $SERVER_USERNAME@$SERVER_IP << EOF
+    cd ${PROJECT_DIRECTORY}
+    docker-compose -f docker-compose.prod.yml pull
+    docker-compose -f docker-compose.prod.yml up -d
+    bash fix_permissions_for_production.sh
+EOF
+
+echo "### DONE ###"
---
+++ fix_permissions_for_production.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose.prod.yml exec -T -u root wordpress chown -R www-data:www-data /var/www/html
